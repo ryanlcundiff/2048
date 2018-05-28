302a42dd34cd52a1033e55020c15c41c26226f12
Ryan <ryanlcundiff@gmail.com>
2018-05-28 10:16:12 -0600

    WE DONE ADDED A MODEL AND VIEW YEET

---
+++ src/litcode/lit2048/ModelLit2048.java
@@ -0,0 +1,5 @@
+package litcode.lit2048;
+
+public class ModelLit2048
+{
+}
---
+++ src/litcode/lit2048/ViewLit2048.java
@@ -0,0 +1,12 @@
+package litcode.lit2048;
+
+public class ViewLit2048
+{
+  public ViewLit2048()
+  {}
+
+  public void display(ModelLit2048 model)
+  {
+    System.out.println("display the model");
+  }
+}
