1ae4575be8b66d3829873075aa200e5b6acf763f
Ryan <ryanlcundiff@gmail.com>
2018-06-14 11:16:02 -0600

    We done made the gucci model own the gucci view

--- src/litcode/lit2048/Main.java
+++ src/litcode/lit2048/Main.java
@@ -1,11 +1,21 @@
 package litcode.lit2048;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws IOException
   {
-    ModelLit2048 model = new ModelLit2048();
     ViewLit2048 view = new ViewLit2048();
-    view.displayBoard(model);
+    ModelLit2048 model = new ModelLit2048(view);
+    while (!model.gameOver())
+    {
+      Scanner s = new Scanner(System.in);
+
+      System.out.print("ENTER YOUR MOVE");
+      //String move = s.next();
+      //System.out.println("MOVE: " + move);
+    }
   }
 }
--- src/litcode/lit2048/ModelLit2048.java
+++ src/litcode/lit2048/ModelLit2048.java
@@ -7,14 +7,23 @@ public class ModelLit2048
 {
   private Random random;
   private int[][] array2d;
+  private ViewLit2048 view;
 
-  public ModelLit2048()
+  public ModelLit2048(ViewLit2048 view)
   {
+    this.view=view;
     random=new Random();
     array2d=new int[4][4];
-    array2d[2][2]=newNumber();
+    placeRandomTile();
+    placeRandomTile();
+    view.displayBoard(this);
   }
 
+  public boolean gameOver()
+  {
+    return false;
+
+  }
   public int numberOfRows()
   {
     return 4;
@@ -24,11 +33,26 @@ public class ModelLit2048
   {
     return array2d[whichRow];
   }
+
+  private void placeRandomTile()
+  {
+   int x;
+   int y;
+    do
+    {
+      x = random.nextInt(4);
+      y = random.nextInt(4);
+    }
+    while(array2d[x][y]>0);
+    array2d[x][y]=newNumber();
+  }
+
+
   private int newNumber()
   {
     int randomNumber=random.nextInt(10);
 
-    if(randomNumber < 7 )
+    if(randomNumber < 6 )
     {
       return 2;
     }
