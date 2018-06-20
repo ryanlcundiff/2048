dcc005df05e3bd065a793057379d4c71fcc6a18d
Ryan <ryanlcundiff@gmail.com>
2018-06-20 09:49:08 -0600

    We done did make it so you can gucci move to the gucci left using the gucci a key in the gucci game

--- src/litcode/lit2048/ModelLit2048.java
+++ src/litcode/lit2048/ModelLit2048.java
@@ -10,7 +10,8 @@ public class ModelLit2048
   private int[][] array2d;
   private ViewLit2048 view;
 
-  public ModelLit2048(ViewLit2048 view)
+
+  protected ModelLit2048(ViewLit2048 view)
   {
     this.view=view;
     quit = false;
@@ -28,8 +29,42 @@ public class ModelLit2048
       return;
     }
     //we need to make this work
+
+    for (int i=0; i<numberOfRows(); i++)
+    {
+      slideRow(row(i));
+      mergeRow(row(i));
+      slideRow(row(i));
+    }
     placeRandomTile();
     view.displayBoard(this);
+  }
+
+  private void slideRow(int[] row)
+  {
+    for (int i=0; i<row.length-1; i++)
+    {
+      for (int j=0; j<row.length-1; j++)
+      {
+        if (row[j] == 0 && row[j+1] !=0)
+        {
+          row[j] = row[j+1];
+          row[j+1] = 0;
+        }
+      }
+    }
+
+  }
+  private void mergeRow(int[] row)
+  {
+    for (int i=0; i<row.length-1; i++)
+    {
+      if (row[i] == row[i+1])
+      {
+         row[i] = 2*row[i];
+         row[i+1] = 0;
+      }
+    }
   }
 
   public boolean gameOver()
